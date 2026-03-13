12e5645367315005f856b238c1beab3367917924
Nikhil-Doal <doalnikhil@gmail.com>
2026-03-12 23:27:28 -0400

    switch to cosine importance sampling for lambertian materials

--- materials/lambertian.cpp
+++ materials/lambertian.cpp
@@ -3,7 +3,10 @@
 Lambertian::Lambertian (std::shared_ptr<Texture> a) : albedo(a) {}
 
 bool Lambertian::scatter(const Ray &ray_in, const HitRecord &rec, Vec3 &attenuation, Ray &scattered) const{
-  Vec3 scatter_direction = rec.normal + random_unit_vector();
+  ONB onb;
+  onb.build_from_w(rec.normal);
+
+  Vec3 scatter_direction = onb.local(random_cosine_direction());
   
   scattered = Ray(rec.point, scatter_direction);
   attenuation = albedo -> value(rec.u, rec.v, rec.point, rec.t);
--- utils/sampling_utils.h
+++ utils/sampling_utils.h
@@ -2,6 +2,22 @@
 #include "../core/vec3.h"
 #include "math_utils.h"
 
+// ONB helper (orthonormal basis)
+struct ONB {
+  Vec3 u, v, w;
+
+  void build_from_w(const Vec3 &n) {
+    w = n.normalize();
+    Vec3 a = (fabs(w.x) > 0.9) ? Vec3(0, 1, 0) : Vec3(1, 0, 0);
+    v = w.cross(a).normalize();
+    u = w.cross(v);
+  }
+
+  Vec3 local(const Vec3 &a) const {
+    return u * a.x + v * a.y + w * a.z;
+  }
+};
+
 inline Vec3 random_in_unit_sphere() {
   while (true) {
     // monte carlo sampling
@@ -12,6 +28,15 @@ inline Vec3 random_in_unit_sphere() {
   }
 }
 
-inline Vec3 random_unit_vector() {
-  return random_in_unit_sphere().normalize();
+// malley's method for cosine sampling
+inline Vec3 random_cosine_direction() {
+  double r1 = random_double();
+  double r2 = random_double();
+
+  double phi = 2 * PI * r1;
+  double x = cos(phi) * sqrt(r2);
+  double y = sin(phi) * sqrt(r2);
+  double z = sqrt(1 - r2);
+
+  return Vec3(x, y, z);
 }
